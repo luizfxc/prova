6f49a932966e256e32b72897a63d30bfb154a8cf
Luiz Felipe <luisfelipesouzapaula@gmail.com>
2025-05-21 11:54:02 -0300

    Iniciando Logica Programacao

---
+++ BACKEND/backend/backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,23 @@
+nome= "Luiz Felipe"
+idade= 18
+salario= 3555.50
+legal= True
+
+print("Nome",nome)
+print("Idade",idade)
+print("Salario",salario)
+print("Legal",legal)
+
+animal= "cavalo"
+nascimento= 2016
+raça= "quartodemilha"
+peso= 346.80
+bravo= "false"
+adestrado= "true"
+
+print("Animal",animal)
+print("Nascimento",nascimento)
+print("Raça",raça)
+print("Peso",peso)
+print("Bravo",bravo)
+print("Adestrado",adestrado)
---
+++ BACKEND/backend/backend/logicaProgramacao/02_operacoesMatematicas.py
@@ -0,0 +1,29 @@
+a = 10
+b = 15
+soma = a + b
+
+subtracao = a - b
+
+divisao = a / b
+
+multiplicacao = a * b
+
+print("a Soma de a + b é:", soma)
+print("a Subtração de a - b é:", subtracao)
+print("a Divisão de a / b é:", divisao)
+print("a Multiplicação de a * b é:", multiplicacao)
+
+leitedeboi = 14.000
+leitedevaca = 25.000
+soma = leitedevaca + leitedeboi
+
+subtracao = leitedevaca - leitedeboi
+
+divisao = leitedevaca / leitedeboi
+
+multiplicacao = leitedevaca * leitedeboi
+
+print("a Soma do leite do sitio 1 + do leite do sitio 3 é:", soma)
+print("a Subtração do leito do sitio 1 - leite do sitio 3 é:", subtracao)
+print("a Divisão do leite do sitio 1 / leite do sitio 3 é:", divisao)
+print("a Multiplicação do leite do sitio 1 * leite do sitio 3 é:", multiplicacao)
