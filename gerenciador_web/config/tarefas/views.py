63293b1c29db5e81bfbfc0ab0b6f0e18bfc1f611
Luiz Felipe Souza de Paula <luisfelipesouzapaula@gmail.com>
2025-09-18 09:49:53 -0300

    atividades urls

--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tarefa
+
+from django.http import HttpResponse
+
+def listar_tarefas(request):
+    tarefa_salvas = Tarefa.objects.all()
+    print(tarefa_salvas)
+    return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
